eba49d46d98e1dcc017cfced469962c173b7fcd0
Mayank <hellomayankashok@gmail.com>
2021-03-21 00:35:32 -0700

    Modify Menu and Menu Card
    Following Done:
    - distributed code in MenuCard Component.
    - Created a MobileMenuCard component for mobile screen size
        -- logic added to Menu component
    
    To be done:
    - create a window.eventListener function to check for screen size on resizing screen size.
        -- show MobileMenuCard for smaller screen size using this function.
    
    -

--- src/components/Menu/MobileMenuCard/MobileMenuCard.js
+++ src/components/Menu/MobileMenuCard/MobileMenuCard.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Aux from '../../../hoc/Auxillary/Auxillary'
 
-import classes from './MenuCard.module.css';
+import classes from './MobileMenuCard.module.css';
 
 // importing images
 import imgVegBiryani from '../../../resources/images/veg_biryani.jpg';
 
 export default function MenuCard(props) {
+
+    const [orderQty, setOrderQty] = useState(0);
+
+    const addOrderQtyHandler = () => {
+        if(orderQty<10){
+            setOrderQty(orderQty + 1);
+        }else{
+            alert('Maximum Order Qty Reached');
+        }        
+    }
+
+    const removeOrderQtyHandler = () => {
+        if(orderQty>0){
+            setOrderQty(orderQty - 1);
+        }
+        else{
+            alert('Order qty cannot be less than zero')
+        }
+        
+    }
+
+
     return (
         <Aux>
             <div className={classes.menu_card}>
@@ -18,8 +40,12 @@ export default function MenuCard(props) {
                     <p className={classes.menu_item_description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                                 Doloribus perferendis nostrum hic quae dolore natus odio assumenda repellendus qui ex?</p>
                     <div className={classes.menu_item_order_div}>
-                        <a className={classes.add_to_btn} href="">Add to plate</a>
-                        <div className={classes.menu_card_order_qty_div}>
+                        <div className={classes.order_qty_div}>
+                            <button className={classes.add_to_btn} onClick={removeOrderQtyHandler}>-</button>
+                            <p>{orderQty}</p>
+                            <button className={classes.add_to_btn} onClick={addOrderQtyHandler}>+</button>
+                        </div>
+                        {/* <div className={classes.menu_card_order_qty_div}>
                             <button>-</button>
                             <select name="select_order_qty" id="select_order_qty">
                                 <option value="1">1</option>
@@ -29,13 +55,13 @@ export default function MenuCard(props) {
                                 <option value="5">5</option>
                             </select>
                             <button>+</button>
-                        </div>
+                        </div> */}
                     </div>
                     <div className={classes.menu_item_description_div}>
                         <p className={classes.see_description_button}>Ingredients</p>
                         <p className={classes.see_description_button}>Description</p>
                     </div>
-                    
+
                 </div>
             </div>
 
--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Auxillary/Auxillary'
+import Menu from '../../components/Menu/Menu';
 
 import classes from './Main.module.css';
-
-// importing images
-import imgVegBiryani from '../../resources/images/veg_biryani.jpg';
 
 export default class Main extends Component {
     render() {
         return (
             <Aux>
                 {/* <!-- Menu Style 2 --> */}
+                <Menu></Menu>
             </Aux>
         )
     }
